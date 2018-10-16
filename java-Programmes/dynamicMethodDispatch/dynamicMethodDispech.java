122cd36be34f35e95cfbd4282cb9fb4033d09f82
Rahul prajapati <33652351+Raarav@users.noreply.github.com>
2018-10-16 10:52:01 -0800

    dynamicMethodDispatch

--- java-Programmes/dynamicMethodDispatch/dynamicMethodDispech.java
+++ java-Programmes/dynamicMethodDispatch/dynamicMethodDispech.java
@@ -6,11 +6,17 @@ public class dynamicMethodDispech {
     public static void main(String args[]) {
 
 
-        A1 bj = new A1();
-        B1 bj1 = new B1();
+        A1 bj = new A1();/*bj object of class A1*/
+        B1 bj1 = new B1();/*bj1 object of class B1*/
 
-        A1 bj2;
+        A1 bj2;/*bj2 refrence of class A1*/
         bj2 = bj;
         bj2.show();
+       
+        B1 bj3;/*bj3 refrence of class B1*/
+        bj3 = bj1;
+        bj3.show();
+
+
     }
 }
